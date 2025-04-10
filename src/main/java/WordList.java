3e9bc8129b5e363efeef4f9fa973291a7410d732
featherfly12 <146142331+featherfly12@users.noreply.github.com>
2025-04-10 16:33:06 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -1,36 +1,38 @@
 import java.util.*;
 
-public class WordList
-{
-  private ArrayList <String> myList;
+public class WordList {
+  private ArrayList<String> myList;
 
-  public WordList() { myList = new ArrayList<String>(); }
-  public void add(String word) { myList.add(word); }
-  public String toString() { return myList.toString(); }
-
-  public int numWordsOfLength(int len)
-  {
-    int count = 0;
-    for(int i = 0;i<myList.size();i++){
-      if(myList.get(i).length()==len){
-        count++;
-      }
-      return count;
-    }
-    
+  public WordList() {
+    myList = new ArrayList<String>();
   }
 
-  public void removeWordsOfLength(int len)
-  {
-    int i = 0;
-    while(i<numWordsOfLength(len)){
-      if(myList.get(i).length()==len){
-        myList.remove(i);
+  public void add(String word) {
+    myList.add(word);
+  }
+
+  public String toString() {
+    return myList.toString();
+  }
+
+  public int numWordsOfLength(int len) {
+    int count = 0;
+    for (int i = 0; i < myList.size(); i++) {
+      if (myList.get(i).length() == len) {
+        count++;
       }
-      else{
+    }
+    return count;
+  }
+
+  public void removeWordsOfLength(int len) {
+    int i = 0;
+    while (i < myList.size()) {
+      if (myList.get(i).length() == len) {
+        myList.remove(i);
+      } else {
         i++;
       }
     }
-
   }
 }
